f66a1ca8dddaf1b4bfde9305c51aee20d6d5ef78
Satya Sai Kiran Adimulam <156501883+kiranadimulam2002@users.noreply.github.com>
2025-06-12 12:20:16 +0530

    Update FreqOfCharUsingMap.java

--- FreqOfCharUsingMap.java
+++ FreqOfCharUsingMap.java
@@ -10,11 +10,6 @@ public class FreqOfCharUsingMap {
 		ArrayList<Character> list = new ArrayList<Character>();
 		list.addAll(Arrays.asList('S', 'A', 'T', 'Y', 'A', 'S', 'A', 'I', 'K', 'I', 'R', 'A', 'N'));
 		
-		int[] arr = new int[256];
-		for (char c : list) {
-			int num = c;
-			arr[num]++;
-		}
 		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 		for(char ch: list) {
 			if (map.containsKey(ch)) {
